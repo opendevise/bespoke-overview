ddcfdc1552f94781c0c1ecb525403b9874cc959e
Dan Allen <dan.j.allen@gmail.com>
2015-08-15 23:13:17 -0600

    disable navigation in overview; use flag to track state
    - disable slide navigation in overview
    - use flag to track state of overview instead of querying class list
    - cleanups

--- dist/bespoke-overview.js
+++ dist/bespoke-overview.js
@@ -8,21 +8,21 @@
 
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var o;"undefined"!=typeof window?o=window:"undefined"!=typeof global?o=global:"undefined"!=typeof self&&(o=self);var n=o;n=n.bespoke||(n.bespoke={}),n=n.plugins||(n.plugins={}),n.overview=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 // TODO
-// - disable navigation when in overview
 // - deactivate current slide?
-// - control scrollbar visibility on timer that matches transition duration of transform property
 // - inject bespoke-overview CSS into presentation
+// - tests
+// - only use overflow div if Chrome + transform scale; or should we always enable?
 module.exports = function(opts) {
   return function(deck) {
     var KEYCODE_O = 79,
     RE_CSV = new RegExp(', *'),
+    overviewClassName = 'bespoke-overview',
     activeSlideIndex = 0,
+    overviewActive = false,
     cols = typeof opts !== 'undefined' && typeof opts.cols !== 'undefined' ? parseInt(opts.cols) : 3,
     margin = typeof opts !== 'undefined' && typeof opts.margin !== 'undefined' ? parseFloat(opts.margin) : 10,
     getTransformScaleFactor = function(element) {
       return element.getBoundingClientRect().width / element.offsetWidth;
-      // equivalent to...
-      //return parseFloat(element.style.transform.match(/scale\(([0-9.]+)\)/)[1]);
     },
     getZoomFactor = function(element) {
       if ('zoom' in element.style) {
@@ -40,8 +40,13 @@ module.exports = function(opts) {
       var selection = deck.slides.indexOf(e.currentTarget);
       if (selection !== -1) exitOverview(selection);
     },
+    isOverviewActive = function() {
+      return overviewActive;
+    },
+    navigate = function() {
+      return !isOverviewActive();
+    },
     activateOverview = function() {
-      // remember active slide
       activeSlideIndex = deck.slide();
 
       var parent = deck.parent,
@@ -63,13 +68,13 @@ module.exports = function(opts) {
       // FIXME only necessary when transform scale is used in Chrome; though maybe we should always add it?
       if ('zoom' in parent.style) {
         var overflow = document.createElement('div');
-        overflow.className = 'bespoke-overflow';
+        overflow.className = overviewClassName;
         overflow.style.cssText = 'position: absolute; top: 100vh; left: 0; height: 1px; width: 100vw; visibility: hidden';
         parent.appendChild(overflow);
       }
 
-      // QUESTION should we add bespoke-overview to html or body element instead?
-      parent.classList.add('bespoke-overview');
+      // QUESTION should we add class to html or body element instead?
+      parent.classList.add(overviewClassName);
       // NOTE we need fine-grained control over scrollbar, so override CSS
       parent.style.overflowY = 'scroll';
 
@@ -119,7 +124,7 @@ module.exports = function(opts) {
 
       // TODO add option for scrollIntoView position (top, bottom, disabled)
       if (activeSlideIndex >= cols) {
-        // Supported in Chrome by enabling "Smooth Scrolling" in chrome://flags.
+        // NOTE supported in Chrome by enabling "Smooth Scrolling" in chrome://flags
         parent.style.scrollBehavior = 'smooth';
         var activeSlide = deck.slides[activeSlideIndex];
         var transitionDuration = getTransitionDurationMs(activeSlide, 'transform');
@@ -133,12 +138,13 @@ module.exports = function(opts) {
           activeSlide.scrollIntoView(true);
         }
       }
+      overviewActive = true;
     },
     exitOverview = function(selectedSlideIndex) {
       var selection = typeof selectedSlideIndex === 'number' ? selectedSlideIndex : activeSlideIndex;
       if (selection !== deck.slide()) deck.slide(selection);
 
-      // NOTE the order of operations here heavily impacts the smoothness of transition
+      // NOTE the order of operations are critical, heavily impact smoothness of transition
       var parent = deck.parent,
         overflow = parent.querySelector('.bespoke-overflow');
       if (overflow) parent.removeChild(overflow);
@@ -151,17 +157,19 @@ module.exports = function(opts) {
         slide.style.marginBottom = '';
         slide.removeEventListener('click', onOverviewClicked, false);
       });
-      parent.classList.remove('bespoke-overview');
+      parent.classList.remove(overviewClassName);
+      overviewActive = false;
     },
     toggleOverview = function() {
-      // QUESTION should we keep a state variable instead to track when overview is active?
-      deck.parent.classList.contains('bespoke-overview') ? exitOverview() : activateOverview();
+      isOverviewActive() ? exitOverview() : activateOverview();
     },
     keydownHandler = function(e) {
       if (e.which === KEYCODE_O) toggleOverview();
     };
 
     document.addEventListener('keydown', keydownHandler, false);
+    deck.on('next', navigate);
+    deck.on('prev', navigate);
   };
 };
 
