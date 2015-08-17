b8ac50c108ef67e789683778209aebbf0ddf355f
Dan Allen <dan.j.allen@gmail.com>
2015-08-16 23:11:27 -0600

    fix scroll to focused slided
    - use transitionend event to scroll after transition is complete
    - switch to using hash to hold keycodes
    - fix jshint violations
    - use pointer-events:none instead of visibility:hidden for overflow div

--- dist/bespoke-overview.js
+++ dist/bespoke-overview.js
@@ -9,10 +9,7 @@
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var o;"undefined"!=typeof window?o=window:"undefined"!=typeof global?o=global:"undefined"!=typeof self&&(o=self);var n=o;n=n.bespoke||(n.bespoke={}),n=n.plugins||(n.plugins={}),n.overview=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 module.exports = function(opts) {
   return function(deck) {
-    var KEYCODE_O = 79,
-    KEYCODE_ENTR = 13,
-    KEYCODE_ESC = 27,
-    RE_CSV = new RegExp(', *'),
+    var KEYCODE = { o: 79, enter: 13, esc: 27 },
     overviewClassName = 'bespoke-overview',
     focusedSlideIndex = 0,
     overviewActive = false,
@@ -27,11 +24,9 @@ module.exports = function(opts) {
         if (zoom.length > 0) return parseFloat(zoom);
       }
     },
-    // TODO use CSS prefixes for -webkit and maybe -ms
-    getTransitionDurationMs = function(element, propertyName) {
-      var styles = getComputedStyle(element);
-        propertyIndex = styles.transitionProperty.split(RE_CSV).indexOf(propertyName);
-      return propertyIndex === -1 ? 0 : parseFloat(styles.transitionDuration.split(RE_CSV)[propertyIndex]) * 1000;
+    hasTransition = function(element) {
+      // TODO use CSS prefixes for -webkit and maybe -ms
+      return getComputedStyle(element).transitionDuration !== '0s';
     },
     onOverviewClicked = function(e) {
       var selection = deck.slides.indexOf(e.currentTarget);
@@ -72,17 +67,16 @@ module.exports = function(opts) {
       }
     },
     activateOverview = function() {
-      focusedSlideIndex = deck.slide();
-
       var parent = deck.parent,
-        firstSlide = deck.slides[0],
-        scaleParent = firstSlide.parentNode,
+        lastSlide = deck.slides[0],
+        focusedSlide = deck.slides[focusedSlideIndex = deck.slide()],
         baseScale,
         isZoomed = false;
-      if (scaleParent.classList.contains('bespoke-scale-parent')) {
-        baseScale = getTransformScaleFactor(scaleParent);
+      // FIXME search ancestors in case it's not direct parent
+      if (lastSlide.parentNode.classList.contains('bespoke-scale-parent')) {
+        baseScale = getTransformScaleFactor(lastSlide.parentNode);
       }
-      else if ((baseScale = getZoomFactor(firstSlide))) {
+      else if ((baseScale = getZoomFactor(lastSlide))) {
         isZoomed = true;
       }
       else {
@@ -94,7 +88,7 @@ module.exports = function(opts) {
       if ('zoom' in parent.style) {
         var overflow = document.createElement('div');
         overflow.className = overviewClassName;
-        overflow.style.cssText = 'position: absolute; top: 100vh; left: 0; height: 1px; width: 100vw; visibility: hidden';
+        overflow.style.cssText = 'position:absolute; top:100vh; left:0; height:1px; width:100vw; pointer-events:none';
         parent.appendChild(overflow);
       }
 
@@ -102,7 +96,7 @@ module.exports = function(opts) {
       parent.classList.add(overviewClassName);
       // NOTE we need fine-grained control over scrollbar, so override CSS
       parent.style.overflowY = 'scroll';
-      // NOTE supported in Chrome by enabling Smooth Scrolling in chrome://flags
+      // NOTE supported in Chrome by enabling smooth scrolling in chrome://flags
       parent.style.scrollBehavior = 'smooth';
 
       var baseMargin = margin / baseScale,
@@ -111,8 +105,8 @@ module.exports = function(opts) {
         scrollbarWidth = ((parent.offsetWidth / baseScale) - deckWidth) / baseScale,
         // FIXME this calculation doesn't take into account margins on parent
         totalGutter = baseMargin * (cols + 1),
-        slideWidth = firstSlide.offsetWidth,
-        slideHeight = firstSlide.offsetHeight,
+        slideWidth = lastSlide.offsetWidth,
+        slideHeight = lastSlide.offsetHeight,
         slideX = (deckWidth - slideWidth) / 2 + scrollbarWidth * (isZoomed ? 1 : baseScale),
         slideY = (deckHeight - slideHeight) / 2,
         scale = (deckWidth - totalGutter) / cols / slideWidth,
@@ -129,9 +123,8 @@ module.exports = function(opts) {
         // TODO use CSS prefixes for -webkit and maybe -ms
         slide.style.transform = 'translate(' + x + 'px, ' + y + 'px) scale(' + scale + ')';
         // HACK for some reason, must kick bottom margin using 0% or else Chrome screws up layout
-        //slide.style.marginBottom = '0%';
         slide.style.marginBottom = '0%';
-        if (col == (cols - 1)) {
+        if (col === (cols - 1)) {
           col = 0;
           row += 1;
         }
@@ -141,28 +134,27 @@ module.exports = function(opts) {
       });
 
       // NOTE add margin to last slide to leave gap below last row; doesn't work in Firefox
-      deck.slides[deck.slides.length - 1].style.marginBottom = scaledMargin + 'px';
+      lastSlide.style.marginBottom = scaledMargin + 'px';
 
-      // deactivate current slide by going to first slide (ideally no slide)
-      //deck.slide(0);
       deck.slides.forEach(function(slide) {
         slide.addEventListener('click', onOverviewClicked, false);
       });
 
       // TODO add option for scrollIntoView position (top, bottom, disabled)
       if (focusedSlideIndex >= cols) {
-        var focusedSlide = deck.slides[focusedSlideIndex];
-        var transitionDuration = getTransitionDurationMs(focusedSlide, 'transform');
-        // TODO use scrollTo so that we leave margin above slide (like first row)
-        if (transitionDuration > 0) {
-          // add slight delay to give transition ample time to complete
-          setTimeout(function() { focusedSlide.scrollIntoView(true); }, transitionDuration + 100);
+        if (hasTransition(lastSlide)) {
+          // TODO extract method (using bind to pass argument)
+          var scrollToSlide = function() {
+            focusedSlide.scrollIntoView(true);
+            this.removeEventListener('transitionend', scrollToSlide, false);  
+          };
+          lastSlide.addEventListener('transitionend', scrollToSlide, false);
         }
         else {
           focusedSlide.scrollIntoView(true);
         }
       }
-      deck.slides[focusedSlideIndex].setAttribute('aria-selected', true);
+      focusedSlide.setAttribute('aria-selected', true);
       overviewActive = true;
     },
     exitOverview = function(selectedSlideIndex) {
@@ -187,15 +179,15 @@ module.exports = function(opts) {
       overviewActive = false;
     },
     toggleOverview = function() {
-      isOverviewActive() ? exitOverview() : activateOverview();
+      return isOverviewActive() ? exitOverview() : activateOverview();
     },
     keydownHandler = function(e) {
       switch(e.which) {
-        case KEYCODE_O:
-        case KEYCODE_ESC:
+        case KEYCODE.o:
+        case KEYCODE.esc:
           toggleOverview();
           break;
-        case KEYCODE_ENTR:
+        case KEYCODE.enter:
           if (isOverviewActive()) exitOverview();
           break;
       }
