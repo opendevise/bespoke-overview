c8081415d838a1a121362a4c496c647aa47aab43
Dan Allen <dan.j.allen@gmail.com>
2015-08-29 02:10:08 -0600

    update Gulp build to publish demo to GitHub Pages

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,17 @@ var gulp = require('gulp'),
   map = require('vinyl-map'),
   istanbul = require('istanbul'),
   karma = require('karma'),
+  concat = require('gulp-concat'),
   header = require('gulp-header'),
   rename = require('gulp-rename'),
+  replace = require('gulp-replace'),
   uglify = require('gulp-uglify'),
   pkg = require('./package.json'),
   browserify = require('browserify'),
   source = require('vinyl-source-stream'),
   buffer = require('vinyl-buffer'),
-  path = require('path');
+  path = require('path'),
+  pages = require('gh-pages');
 
 gulp.task('default', ['clean', 'lint', 'test', 'compile']);
 gulp.task('dev', ['compile', 'lint', 'test', 'watch']);
@@ -22,7 +25,7 @@ gulp.task('watch', function() {
 });
 
 gulp.task('clean', function(done) {
-  del(['dist', 'lib-instrumented', 'test/coverage'], done);
+  del(['dist', 'demo/dist', 'lib-instrumented', 'test/coverage'], done);
 });
 
 gulp.task('lint', function() {
@@ -73,4 +76,36 @@ gulp.task('compile', ['clean'], function() {
       '<%= license %> License */\n'
     ].join(''), pkg))
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('compile:demo', ['compile:demo:css', 'compile:demo:html', 'compile:demo:js']);
+
+gulp.task('compile:demo:css', ['clean'], function() {
+  return gulp.src('demo/style.css')
+    .pipe(gulp.dest('demo/dist'));
+});
+
+gulp.task('compile:demo:html', ['clean'], function() {
+  return gulp.src('demo/index.html')
+    .pipe(replace(/<script [\s\S]*<\/script>/, '<script src="build.js"></script>'))
+    .pipe(gulp.dest('demo/dist'));
+});
+
+gulp.task('compile:demo:js', ['compile'], function() {
+  return gulp.src([
+      'node_modules/bespoke/dist/bespoke.js',
+      'node_modules/bespoke-classes/dist/bespoke-classes.js',
+      'node_modules/bespoke-keys/dist/bespoke-keys.js',
+      'node_modules/bespoke-touch/dist/bespoke-touch.js',
+      'node_modules/bespoke-scale/dist/bespoke-scale.js',
+      'dist/bespoke-overview.js',
+      'demo/demo.js'
+  ])
+  .pipe(concat('build.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('demo/dist'));
+});
+
+gulp.task('deploy', ['compile:demo'], function(done) {
+  pages.publish(path.join(__dirname, 'demo/dist'), {}, done);
 });
