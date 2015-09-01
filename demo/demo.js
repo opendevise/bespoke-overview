3f081d5592797e1d8a19c3d1724a28676bc3ac51
Dan Allen <dan.j.allen@gmail.com>
2015-09-01 17:36:18 -0600

    get demo working in IE10/11

--- demo/demo.js
+++ demo/demo.js
@@ -1,8 +1,10 @@
-var deck = bespoke.from('.deck', [
+var parent = document.querySelector('.deck');
+var deck = bespoke.from(parent, [
   bespoke.plugins.classes(),
   bespoke.plugins.keys(),
   bespoke.plugins.touch(),
-  bespoke.plugins.scale(),
+  //bespoke.plugins.scale(),
+  bespoke.plugins.scale('webkitAppearance' in parent.style ? 'zoom' : 'transform'),
   //bespoke.plugins.scale('transform'),
   bespoke.plugins.overview()
   //bespoke.plugins.overview({ columns: 2, margin: 10, autostart: true, title: true, numbers: true })
