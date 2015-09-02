33ac11b6fc363c83f39539eb887d774fa3359214
Dan Allen <dan.j.allen@gmail.com>
2015-09-01 22:48:53 -0600

    cleanup deck configuration slightly

--- demo/demo.js
+++ demo/demo.js
@@ -1,14 +1,14 @@
-var parent = document.querySelector('.deck');
-var deck = bespoke.from(parent, [
-  bespoke.plugins.classes(),
-  bespoke.plugins.keys(),
-  bespoke.plugins.touch(),
-  //bespoke.plugins.scale(),
-  bespoke.plugins.scale('webkitAppearance' in parent.style ? 'zoom' : 'transform'),
-  //bespoke.plugins.scale('transform'),
-  bespoke.plugins.overview()
-  //bespoke.plugins.overview({ columns: 2, margin: 10, autostart: true, title: true, numbers: true })
-]);
+var parent = document.querySelector('.deck'),
+  deck = bespoke.from(parent, [
+    bespoke.plugins.classes(),
+    bespoke.plugins.keys(),
+    bespoke.plugins.touch(),
+    //bespoke.plugins.scale(),
+    bespoke.plugins.scale('webkitAppearance' in parent.style ? 'zoom' : 'transform'),
+    //bespoke.plugins.scale('transform'),
+    bespoke.plugins.overview()
+    //bespoke.plugins.overview({ columns: 2, margin: 10, autostart: true, title: true, numbers: true })
+  ]);
 
 // expose API to other applications
 window.deck = deck;
