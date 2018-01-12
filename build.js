862bf518b396ec64d29bac64c6b8ffab3bd0ad0c
Dan Allen <dan.j.allen@gmail.com>
2018-01-12 01:23:29 -0700

    Updates

--- build.js
+++ build.js
@@ -1,15 +1,15 @@
 /*!
  * Bespoke.js v1.0.0
  *
- * Copyright 2014, Mark Dalgleish
+ * Copyright 2015, Mark Dalgleish
  * This content is released under the MIT license
  * http://mit-license.org/markdalgleish
  */
 
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.bespoke=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
-var from = function(selectorOrElement, plugins) {
-  var parent = selectorOrElement.nodeType === 1 ? selectorOrElement : document.querySelector(selectorOrElement),
-    slides = [].filter.call(parent.children, function(el) { return el.nodeName !== 'SCRIPT'; }),
+var from = function(opts, plugins) {
+  var parent = (opts.parent || opts).nodeType === 1 ? (opts.parent || opts) : document.querySelector(opts.parent || opts),
+    slides = [].filter.call(typeof opts.slides === 'string' ? parent.querySelectorAll(opts.slides) : (opts.slides || parent.children), function(el) { return el.nodeName !== 'SCRIPT'; }),
     activeSlide = slides[0],
     listeners = {},
 
@@ -39,12 +39,11 @@ var from = function(selectorOrElement, plugins) {
 
     on = function(eventName, callback) {
       (listeners[eventName] || (listeners[eventName] = [])).push(callback);
+      return off.bind(null, eventName, callback);
+    },
 
-      return function() {
-        listeners[eventName] = listeners[eventName].filter(function(listener) {
-          return listener !== callback;
-        });
-      };
+    off = function(eventName, callback) {
+      listeners[eventName] = (listeners[eventName] || []).filter(function(listener) { return listener !== callback; });
     },
 
     fire = function(eventName, eventData) {
@@ -63,6 +62,7 @@ var from = function(selectorOrElement, plugins) {
 
     deck = {
       on: on,
+      off: off,
       fire: fire,
       slide: slide,
       next: step.bind(null, 1),
@@ -87,7 +87,6 @@ module.exports = {
 },{}]},{},[1])
 (1)
 });
-
 /*!
  * bespoke-classes v1.0.0
  *
@@ -136,9 +135,9 @@ module.exports = function() {
 (1)
 });
 /*!
- * bespoke-nav v1.0.1
+ * bespoke-nav v1.0.2
  *
- * Copyright 2015, Dan Allen
+ * Copyright 2016, Dan Allen
  * This content is released under the MIT license
  */
 
@@ -156,15 +155,15 @@ module.exports = function(opts) {
 },{"bespoke-nav-kbd":2,"bespoke-nav-touch":3}],2:[function(require,module,exports){
 module.exports = function() {
   return function(deck) {
-    var KEY_SB = 32, KEY_PGUP = 33, KEY_PGDN = 34, KEY_END = 35, KEY_HME = 36,
-        KEY_LT = 37, KEY_RT = 39, KEY_H = 72, KEY_L = 76,
-      isModifierPressed = function(e, keyCode) {
-        return e.ctrlKey || (e.shiftKey && keyCode !== KEY_SB) || e.altKey || e.metaKey;
+    var KEY_SP = 32, KEY_PGUP = 33, KEY_PGDN = 34, KEY_END = 35, KEY_HME = 36,
+        KEY_LT = 37, KEY_RT = 39, KEY_H = 72, KEY_L = 76, KD = 'keydown',
+      modified = function(e, k) {
+        return e.ctrlKey || (e.shiftKey && (k === KEY_HME || k === KEY_END)) || e.altKey || e.metaKey;
       },
-      onKeydown = function(e) {
-        if (!isModifierPressed(e, e.which)) {
+      onKey = function(e) {
+        if (!modified(e, e.which)) {
           switch(e.which) {
-            case KEY_SB: return e.shiftKey ? deck.prev() : deck.next();
+            case KEY_SP: return (e.shiftKey ? deck.prev : deck.next)();
             case KEY_RT: case KEY_PGDN: case KEY_L: return deck.next();
             case KEY_LT: case KEY_PGUP: case KEY_H: return deck.prev();
             case KEY_HME: return deck.slide(0);
@@ -172,35 +171,28 @@ module.exports = function() {
           }
         }
       };
-    deck.on('destroy', function() { document.removeEventListener('keydown', onKeydown); });
-    document.addEventListener('keydown', onKeydown);
+    deck.on('destroy', function() { document.removeEventListener(KD, onKey); });
+    document.addEventListener(KD, onKey);
   };
 };
 
 },{}],3:[function(require,module,exports){
-module.exports = function(opts) {
+module.exports = function(options) {
   return function(deck) {
-    opts = opts || {};
-    var TOUCHSTART = 'touchstart', TOUCHMOVE = 'touchmove', start = null,
-      axis = 'page' + (opts.axis && ['x', 'y'].indexOf(opts.axis) !== -1 ? opts.axis.toUpperCase() : 'X'),
-      gap = (typeof opts.threshold === 'number' ? Math.abs(opts.threshold) : Math.ceil(50 / window.devicePixelRatio)),
-      onTouchstart = function(e) {
-        if (e.touches.length === 1) start = e.touches[0][axis];
-      },
-      onTouchmove = function(e) {
-        if (start === null) return;
-        var delta = e.touches[0][axis] - start;
-        if (Math.abs(delta) > gap) {
-          deck[delta > 0 ? 'prev' : 'next']();
-          start = null;
+    var opts = (options || {}), TS = 'touchstart', TM = 'touchmove', ADD = 'addEventListener', RM = 'removeEventListener',
+      src = deck.parent, start = null, delta = null, axis = 'page' + (opts.axis === 'y' ? 'Y' : 'X'),
+      gap = typeof opts.threshold === 'number' ? opts.threshold : 50 / window.devicePixelRatio,
+      onStart = function(e) { start = e.touches.length === 1 ? e.touches[0][axis] : null; },
+      onMove = function(e) {
+        if (start === null) return; // not ours
+        if (start === undefined) return e.preventDefault(); // action already taken
+        if (Math.abs(delta = e.touches[0][axis] - start) > gap) {
+          (delta > 0 ? deck.prev : deck.next)();
+          start = e.preventDefault(); // mark action taken
         }
       };
-    deck.on('destroy', function() {
-      deck.parent.removeEventListener(TOUCHSTART, onTouchstart);
-      deck.parent.removeEventListener(TOUCHMOVE, onTouchmove);
-    });
-    deck.parent.addEventListener(TOUCHSTART, onTouchstart);
-    deck.parent.addEventListener(TOUCHMOVE, onTouchmove);
+    deck.on('destroy', function() { src[RM](TS, onStart); src[RM](TM, onMove); });
+    src[ADD](TS, onStart); src[ADD](TM, onMove);
   };
 };
 
@@ -265,25 +257,30 @@ module.exports = function(options) {
 (1)
 });
 /*!
- * bespoke-overview v1.0.4
+ * bespoke-overview v1.0.5
  *
- * Copyright 2015, Dan Allen
+ * Copyright 2018, Dan Allen
  * This content is released under the MIT license
  */
 
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g=(g.bespoke||(g.bespoke = {}));g=(g.plugins||(g.plugins = {}));g.overview = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var cssInserted = 0;
 module.exports = function(opts) {
-  require('insert-css')('.bespoke-parent.bespoke-overview{pointer-events:auto}' +
-    '.bespoke-overview :not(img){pointer-events:none}' +
-    '.bespoke-overview .bespoke-slide{opacity:1;visibility:visible;cursor:pointer;pointer-events:auto}' +
-    '.bespoke-overview .bespoke-active{outline:6px solid #cfd8dc;outline-offset:-3px;-moz-outline-radius:3px}' +
-    '.bespoke-overview .bespoke-bullet{opacity:1;visibility:visible}' +
-    '.bespoke-overview-counter{counter-reset:overview}' +
-    '.bespoke-overview-counter .bespoke-slide::after{counter-increment:overview;content:counter(overview);position:absolute;right:.75em;bottom:.5em;font-size:1.25rem;line-height:1.25}' +
-    '.bespoke-title{visibility:hidden;position:absolute;top:0;left:0;width:100%;pointer-events:auto}' +
-    '.bespoke-title h1{margin:0;font-size:1.6em;line-height:1.2;text-align:center}' +
-    '.bespoke-overview:not(.bespoke-overview-to) .bespoke-title{visibility:visible}' +
-    '.bespoke-overview-to .bespoke-active,.bespoke-overview-from .bespoke-active{z-index:1}', { prepend: true });
+  if (cssInserted++ === 0) (function(head, style) {
+    (style = document.createElement('style')).textContent = '.bespoke-parent.bespoke-overview{pointer-events:auto}\n' +
+        '.bespoke-overview *{pointer-events:none}\n' +
+        '.bespoke-overview img{-moz-user-select:none}\n' +
+        '.bespoke-overview .bespoke-slide{opacity:1;visibility:visible;cursor:pointer;overflow:hidden;pointer-events:auto}\n' +
+        '.bespoke-overview .bespoke-active{outline:6px solid #cfd8dc;outline-offset:-3px;-moz-outline-radius:3px}\n' +
+        '.bespoke-overview .bespoke-bullet{opacity:1;visibility:visible}\n' +
+        '.bespoke-overview-counter{counter-reset:overview}\n' +
+        '.bespoke-overview-counter .bespoke-slide::after{counter-increment:overview;content:counter(overview);position:absolute;right:.75em;bottom:.5em;font-size:1.25rem;line-height:1.25}\n' +
+        '.bespoke-title{visibility:hidden;position:absolute;top:0;left:0;width:100%;pointer-events:auto}\n' +
+        '.bespoke-title h1{margin:0;font-size:1.6em;line-height:1.2;text-align:center}\n' +
+        '.bespoke-overview:not(.bespoke-overview-to) .bespoke-title{visibility:visible}\n' +
+        '.bespoke-overview-to .bespoke-active,.bespoke-overview-from .bespoke-active{z-index:1}';
+    head.insertBefore(style, head.firstChild);
+  })(document.head);
   return function(deck) {
     opts = typeof opts === 'object' ? opts : {};
     var KEY_O = 79, KEY_ENT = 13, KEY_UP = 38, KEY_DN = 40,
@@ -314,7 +311,7 @@ module.exports = function(opts) {
           style = getComputedStyle(element),
           transitionProperty = style[getStyleProperty(element, 'transitionProperty')];
         if (!transitionProperty || RE_NONE.test(transitionProperty)) return result;
-        // NOTE beyond this point, assume computed style returns compliant values
+        // NOTE assume computed style returns compliant values beyond this point
         transitionProperty = transitionProperty.split(RE_CSV);
         var transitionDuration = style[getStyleProperty(element, 'transitionDuration')].split(RE_CSV),
           transitionDelay = style[getStyleProperty(element, 'transitionDelay')].split(RE_CSV);
@@ -325,7 +322,7 @@ module.exports = function(opts) {
       },
       flushStyle = function(element, property, from, to) {
         if (property) element.style[property] = from;
-        element.offsetHeight; // jshint ignore: line
+        var _ = element.offsetHeight; // jshint ignore:line
         if (property) element.style[property] = to;
       },
       onReady = function() {
@@ -537,7 +534,7 @@ module.exports = function(opts) {
         }
       },
       toggleOverview = function() {
-        overviewActive ? closeOverview() : openOverview(); // jshint ignore:line
+        (overviewActive ? closeOverview : openOverview)();
       },
       onKeydown = function(e) {
         if (e.which === KEY_O) {
@@ -563,30 +560,6 @@ module.exports = function(opts) {
     deck.on('overview', toggleOverview);
     document.addEventListener('keydown', onKeydown, false);
   };
-};
-
-},{"insert-css":2}],2:[function(require,module,exports){
-var inserted = {};
-
-module.exports = function (css, options) {
-    if (inserted[css]) return;
-    inserted[css] = true;
-    
-    var elem = document.createElement('style');
-    elem.setAttribute('type', 'text/css');
-
-    if ('textContent' in elem) {
-      elem.textContent = css;
-    } else {
-      elem.styleSheet.cssText = css;
-    }
-    
-    var head = document.getElementsByTagName('head')[0];
-    if (options && options.prepend) {
-        head.insertBefore(elem, head.childNodes[0]);
-    } else {
-        head.appendChild(elem);
-    }
 };
 
 },{}]},{},[1])(1)
